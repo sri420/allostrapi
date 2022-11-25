3d5fcd1f7c259000217f88f72fa1b1a0fa5282ac
Sridhar <sridharb.in@gmail.com>
2022-11-25 23:42:26 +0530

    changes

---
+++ config/styles.js
@@ -0,0 +1,12 @@
+// styles.js
+const styles = () =>`
+  .ck.ck-editor__main .ck-blurred{
+    max-height: 2200px;
+    max-width: 2200px;
+  }
+  .ck.ck-editor__main .ck-focused{
+    max-height: 500px;
+    max-width: 2200px;
+  }        
+`
+module.exports = styles;
